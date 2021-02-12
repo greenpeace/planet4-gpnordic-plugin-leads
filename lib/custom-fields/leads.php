653540ebc94be3c4e6e84896c26afdfad6b1aa4c
Danny <Gingerdude@users.noreply.github.com>
2021-02-12 18:09:20 +0200

    Replacing "Leads" with "Petition"

--- lib/custom-fields/leads.php
+++ lib/custom-fields/leads.php
@@ -704,7 +704,7 @@ if (function_exists('acf_add_local_field_group')) :
 
     acf_add_local_field_group(array(
         'key' => 'group_5f969db2d4824',
-        'title' => 'Leads Form Block',
+        'title' => 'Petition Form Block',
         'fields' => array(
             array(
                 'key' => 'field_5f969dceef2b3',
