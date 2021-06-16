30388ac59001a04499873dd3188b32e9f9936bdc
svilena <svilena_koleva@yahoo.com>
2021-06-16 17:05:28 +0200

    Donation options GA Tracking added
    Toggle donation amount setting moved to TY page options, tracking set up

--- templates/blocks/leads-form/leads-form.php
+++ templates/blocks/leads-form/leads-form.php
@@ -235,13 +235,13 @@ $url = get_the_permalink();
                     <?php echo $thank_you_settings['donate_headline']; ?>
                 </h4>
                 <?php echo $thank_you_settings['donate_description']; ?>
-                <div class="donate-container">
-                  <?php if ($form_settings['enable_donation_amount']) : ?>
-                    <div class="input-container">
-                        <input class="ghost" type="number" :min="blockData.donateMinimumAmount" pattern="[0-9]*" v-model="donateAmount" @keypress="numbersOnly($event)" @change="checkMinVal($event)"> <span class="currency"><?php echo $form_fields_translations['donate_currency']; ?></span>
+                <div id="donate-container" class="donate-container">
+                  <?php if ($thank_you_settings['enable_donation_amount']) : ?>
+                    <div id="donate-input-amount" class="input-container">
+                        <input id="ghost" class="ghost donation-options" type="number" :min="blockData.donateMinimumAmount" pattern="[0-9]*" v-model="donateAmount" @keypress="numbersOnly($event)" @change="checkMinVal($event)"> <span class="currency"><?php echo $form_fields_translations['donate_currency']; ?></span>
                     </div>
                     <?php endif; ?>
-                    <a id="donate-button" :href="getDonateUrl(`<?php echo $thank_you_settings['donate_url']; ?>`)" class="button button--submit" target="_blank"><?php svg_icon('gift'); ?><?php echo $thank_you_settings['donate_cta']; ?></a>
+                    <a id="donate-button" :href="getDonateUrl(`<?php echo $thank_you_settings['donate_url']; ?>`)" class="button--submit button donation-options" target="_blank"><?php svg_icon('gift'); ?><?php echo $thank_you_settings['donate_cta']; ?></a>
                 </div>
             </div>
         </div>
