c1c75e70722f660c82afd108a34b3c25164ac932
elle <svilena_koleva@yahoo.com>
2022-05-31 12:20:52 +0200

    Displaying page link (from petition overview section)

--- lib/post-types/leads-form.php
+++ lib/post-types/leads-form.php
@@ -41,33 +41,76 @@ function leads_custom_column($column, $post_id)
       echo get_post_meta($post_id, 'form_settings_source_code', true);
       break;
     case 'live':
-      echo get_post_status($post_id) == 'publish' ? '<span style="color:darkgreen;">Yes</span>' : '<span style="color:darkorange;">No</span>';
+      echo get_post_status($post_id) == 'publish' ? '<span style="color:green;">Yes</span>' : '<span style="color:darkorange;">No</span>';
       break;
     case 'included':
-      $pages = get_pages();
-      $page_ids = array();
-      foreach ($pages as $page) {
-        $page_ids[] = $page->ID;
-        $page_title = $page->post_title;
-        $page_content = $page->post_content;
-        $page_id = $page->ID;
-        $page_status = $page->post_status;
-        if ($page_status == 'publish') {
-          $page_status = '<small style="padding-left:0.2rem;"> (Published) </small>';
-        } else if ($page_status == 'draft') {
-          $page_status = '<span style="padding-left:0.2rem;"><small> (Draft) </small></span>';
-        } else {
-          $page_status = '<span style="padding-left:0.2rem;"><small> (Pending review) </small></span>';
-        }
-        $page_content = $page->post_content;
-        $page_permalink = get_permalink($page_id);
-        $page_meta = get_post_meta($page_id);
-        //parse the contents
-        $page_content = apply_filters('the_content', $page_content);
-          //if the post id is equal to the 'data-form-id' value
-          if (strpos($page_content, 'data-form-id="' . $post_id . '"') !== false) {
-            echo '<strong><a href="' . $page_permalink . '" target="_blank">' . $page_title . '</a></strong>' . $page_status . '</br>';
+      $page_ids = array('post_status' => array('draft', 'pending', 'future', 'private', 'publish'));
+      $pages = get_pages($page_ids);
+
+      if (is_array($pages) || is_object($pages))
+      {
+        foreach ($pages as $page) {
+          $page_ids[] = $page->ID;
+          $page_title = $page->post_title;
+          $page_content = $page->post_content;
+          $page_id = $page->ID;
+          $page_status = get_post_status($page_id, 'any');
+          $page_edit_link = get_edit_post_link($page_id);
+
+          //add the page status to the array
+          $page_ids[] = $page_status;
+          $page_ids[] = $page_edit_link;
+
+          $user = is_user_logged_in() ? wp_get_current_user() : null;
+          //returns true if the user is allowed to see the page
+          $user_can_see_page = user_can($user, 'read_post', $page_id);
+          if ($user_can_see_page) {
+            $page_ids[] = $page_id;
           }
+          //check and add it to the array
+          if ($user_can_see_page !== null && $page_status == 'publish' || $page_status == 'draft' || $page_status == 'pending' || $page_status == 'future' || $page_status == 'private') {
+            $page_ids[] = $page_id;
+          }
+          //switch case the post status
+          switch ($page_status) {
+            case $page_status == 'draft':
+              $page_status = '<span style="padding-left:0.2rem;color:darkorange;"><small> (Draft) </small></span>';
+              break;
+
+            case $page_status == 'publish':
+              $page_status = '<span style="padding-left:0.2rem;color:green;"><small> (Published) </small></span>';
+              break;
+
+            case $page_status == 'pending':
+              $page_status = '<span style="padding-left:0.2rem;color:darkorange;"><small> (Pending) </small></span>';
+              break;
+
+            case $page_status == 'future':
+              $page_status = '<span style="padding-left:0.2rem;color:green;"><small> (Scheduled) </small></span>';
+              break;
+
+            case $page_status == 'private':
+              $page_status = '<span style="padding-left:0.2rem;color:darkorange;"><small> (Private) </small></span>';
+              break;
+
+            default:
+              # code...
+              break;
+          }
+
+          // $page_content = $page->post_content;
+          $page_permalink = get_permalink($page_id);
+          $page_meta = get_post_meta($page_id);
+          //parse the contents
+          $page_content = apply_filters('the_content', $page_content);
+          //if the post id is equal to the 'data-form-id' value - works for published pages only
+          if (strpos($page_content, 'data-form-id="' . $post_id . '"') !== false) {
+            $font_end = '<strong><a href="' . $page_permalink . '" target="_blank">' . $page_title . '</a></strong>' . $page_status . '</br>';
+            // $back_end = '<strong><a href="' . $page_edit_link . '" target="_blank"> Editor page: ' . $page_title . '</a></strong></br></br>';
+            echo $font_end;
+            // echo $back_end;
+          }
+        }
       }
     break;
   }
