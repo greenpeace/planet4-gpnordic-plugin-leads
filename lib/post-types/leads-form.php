efae67e6bdaf31c4878018201e199c1432b9d2bd
elle <svilena_koleva@yahoo.com>
2022-06-03 12:06:19 +0200

    Displaying page link (from petition overview section)- refr.code page attr and post array comparison variation

--- lib/post-types/leads-form.php
+++ lib/post-types/leads-form.php
@@ -50,27 +50,18 @@ function leads_custom_column($column, $post_id)
       if (is_array($pages) || is_object($pages))
       {
         foreach ($pages as $page) {
-          $page_ids[] = $page->ID;
+          //add to the page_ids array the page id, title, content, permalink, edit page link, page meta, and post id (for the delete link)
+          $page_id = $page->ID;
           $page_title = $page->post_title;
           $page_content = $page->post_content;
-          $page_id = $page->ID;
+          $page_permalink = get_permalink($page->ID);
+          $page_edit_link = get_edit_post_link($page->ID);
+          $page_page_meta = get_post_meta($page->ID);
+          $page_post_id = $post_id;
           $page_status = get_post_status($page_id, 'auto-draft', 'draft', 'publish', 'pending', 'future', 'private', 'inherit');
-          $page_edit_link = get_edit_post_link($page_id);
+          //parse the contents
+          $page_content = apply_filters('the_content', $page_content);
 
-          //add the page status to the array
-          $page_ids[] = $page_status;
-          $page_ids[] = $page_edit_link;
-
-          $user = is_user_logged_in() ? wp_get_current_user() : null;
-          //returns true if the user is allowed to see the page
-          $user_can_see_page = user_can($user, 'read_post', $page_id);
-          if ($user_can_see_page) {
-            $page_ids[] = $page_id;
-          }
-          //check and add it to the array
-          if ($user_can_see_page !== null && $page_status == 'auto-draft' || $page_status == 'draft' || $page_status == 'publish' || $page_status == 'pending' || $page_status == 'future' || $page_status == 'private' || $page_status == 'inherit' ) {
-            $page_ids[] = $page_id;
-          }
           //switch case the post status
           switch ($page_status) {
             case $page_status == 'auto-draft':
@@ -102,19 +93,39 @@ function leads_custom_column($column, $post_id)
               break;
           }
 
-          // $page_content = $page->post_content;
-          $page_permalink = get_permalink($page_id);
-          $page_meta = get_post_meta($page_id);
-          //parse the contents
-          $page_content = apply_filters('the_content', $page_content);
-          //if the post id is equal to the 'data-form-id' value and display it
-          if (strpos($page_content, 'data-form-id="' . $post_id . '"') !== false) {
-            $font_end = '<strong><a href="' . $page_permalink . '" target="_blank">' . $page_title . '</a></strong>' . $page_status . '</br>';
-            // $back_end = '<strong><a href="' . $page_edit_link . '" target="_blank"> Editor page: ' . $page_title . '</a></strong></br></br>';
-            echo $font_end;
-            // echo $back_end;
+          $dom = new \DOMDocument();
+          @$dom->loadHTML($page_content);
+          $xpath = new \DOMXPath($dom);
+          $page_attrs = $xpath->query('//div[@data-form-id]');
+          //convert the $page_attrs to an array and then loop through it
+          $page_attrs_array = array();
+          foreach ($page_attrs as $page_attr) {
+            $page_attrs_array[] = $page_attr->getAttribute('data-form-id');
           }
+          //if the current post id is in the array then list the page
+          if (in_array($post_id, $page_attrs_array)) {
+            echo '<a href="' . $page_permalink . '" target="_blank">' . $page_title . '</a>' . $page_status . '<br>';
+          }
+
+          //loop throught the post_id array and if the current post_id matcher the page_attrs_array then list the page
+          // foreach ($page_attrs_array as $page_attr_array) {
+          //   if ($post_id == $page_attr_array) {
+          //     echo '<a href="' . $page_permalink . '" target="_blank">' . $page_title . '</a>' . $page_status . '<br>';
+          //   }
+          // }
+
+          //if the current post id value is equal to the 'data-form-id' value display it
+          // if (strpos($page_content, 'data-form-id="' . $page_attrs . '"') == $post_id) {
+          //   // echo 'Form id: ' . $post_id . '<br>';
+          //   $font_end = '<strong><a href="' . $page_permalink . '" target="_blank">' . $page_title . '</a></strong>' . $page_status . '</br>';
+          //   // $back_end = '<strong><a href="' . $page_edit_link . '" target="_blank"> Editor page: ' . $page_title . '</a></strong></br></br>';
+          //   echo $font_end;
+          //   // echo $back_end;
+          // }
         }
+      }
+      else {
+        exit;
       }
     break;
   }
