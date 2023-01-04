912d78b0f6328afa91e0f8717002482f0db77e44
elle <svilena_koleva@yahoo.com>
2023-01-04 10:13:30 +0100

    fix: Refractor code to speed up petition page link aggregation

--- lib/post-types/leads-form.php
+++ lib/post-types/leads-form.php
@@ -44,91 +44,53 @@ function leads_custom_column($column, $post_id)
       echo get_post_status($post_id) == 'publish' ? '<span style="color:green;">Yes</span>' : '<span style="color:darkorange;">No</span>';
       break;
     case 'included':
-      $page_ids = array('post_status' => array('auto-draft', 'draft', 'publish', 'pending', 'future', 'private', 'inherit'));
-      $pages = get_pages($page_ids);
+      $page_ids = array('post_status' => array('draft', 'pending', 'future', 'private', 'publish'));
+      $cache_key = 'pages_with_post_id_' . $post_id;
+      $pages = wp_cache_get($cache_key);
+      if (false === $pages) {
+        $pages = get_pages($page_ids);
+        wp_cache_set($cache_key, $pages);
+      }
+      $all_pages = count($pages);
+      $page_statuses = [];
+      $page_contents = [];
 
-      if (is_array($pages) || is_object($pages))
-      {
-        foreach ($pages as $page) {
-          //add to the page_ids array the page id, title, content, permalink, edit page link, page meta, and post id (for the delete link)
+
+      $status_labels = [
+        'draft' => '<span style="padding-left:0.2rem;color:darkorange;"><small> (Draft) </small></span>',
+        'publish' => '<span style="padding-left:0.2rem;color:green;"><small> (Published) </small></span>',
+        'pending' => '<span style="padding-left:0.2rem;color:darkorange;"><small> (Pending) </small></span>',
+        'future' => '<span style="padding-left:0.2rem;color:green;"><small> (Scheduled) </small></span>',
+        'private' => '<span style="padding-left:0.2rem;color:darkorange;"><small> (Private) </small></span>'
+      ];
+
+      if (is_array($pages) || is_object($pages)) {
+        for ($i = 0; $i < $all_pages; $i++) {
+          $page = $pages[$i];
           $page_id = $page->ID;
           $page_title = $page->post_title;
           $page_content = $page->post_content;
-          $page_permalink = get_permalink($page->ID);
-          $page_edit_link = get_edit_post_link($page->ID);
-          $page_page_meta = get_post_meta($page->ID);
-          $page_post_id = $post_id;
-          $page_status = get_post_status($page_id, 'auto-draft', 'draft', 'publish', 'pending', 'future', 'private', 'inherit');
-          //parse the contents
-          $page_content = apply_filters('the_content', $page_content);
+          $page_status = get_post_status($page_id, 'any');
+          $page_edit_link = get_edit_post_link($page_id);
 
-          //switch case the post status
-          switch ($page_status) {
-            case $page_status == 'auto-draft':
-              $page_status = '<span style="padding-left:0.2rem;color:darkorange;"><small> (Draft) </small></span>';
-              break;
-
-            case $page_status == 'draft':
-              $page_status = '<span style="padding-left:0.2rem;color:darkorange;"><small> (Draft) </small></span>';
-              break;
-
-            case $page_status == 'publish':
-              $page_status = '<span style="padding-left:0.2rem;color:green;"><small> (Published) </small></span>';
-              break;
-
-            case $page_status == 'pending':
-              $page_status = '<span style="padding-left:0.2rem;color:darkorange;"><small> (Pending) </small></span>';
-              break;
-
-            case $page_status == 'future':
-              $page_status = '<span style="padding-left:0.2rem;color:green;"><small> (Scheduled) </small></span>';
-              break;
-
-            case $page_status == 'private':
-              $page_status = '<span style="padding-left:0.2rem;color:darkorange;"><small> (Private) </small></span>';
-              break;
-
-            default:
-              $page_status = '<span style="padding-left:0.2rem;color:darkorange;"><small> (Inherit) </small></span>';
-              break;
+          if (!array_key_exists($page_id, $page_statuses)) {
+          $page_statuses[$page_id] = $page_status;
+          }
+          if (!array_key_exists($page_id, $page_contents)) {
+          $page_contents[$page_id] = apply_filters('the_content', $page_content);
           }
 
-          $dom = new \DOMDocument();
-          @$dom->loadHTML($page_content);
-          $xpath = new \DOMXPath($dom);
-          $page_attrs = $xpath->query('//div[@data-form-id]');
-          //convert the $page_attrs to an array and then loop through it
-          $page_attrs_array = array();
-          foreach ($page_attrs as $page_attr) {
-            $page_attrs_array[] = $page_attr->getAttribute('data-form-id');
-          }
-          //if the current post id is in the array then list the page
-          if (in_array($post_id, $page_attrs_array)) {
-            echo '<a href="' . $page_permalink . '" target="_blank">' . $page_title . '</a>' . $page_status . '<br>';
-          }
+          $page_permalink = get_permalink($page_id);
+          $page_meta = get_post_meta($page_id);
 
-          //loop throught the post_id array and if the current post_id matcher the page_attrs_array then list the page
-          // foreach ($page_attrs_array as $page_attr_array) {
-          //   if ($post_id == $page_attr_array) {
-          //     echo '<a href="' . $page_permalink . '" target="_blank">' . $page_title . '</a>' . $page_status . '<br>';
-          //   }
-          // }
-
-          //if the current post id value is equal to the 'data-form-id' value display it
-          // if (strpos($page_content, 'data-form-id="' . $page_attrs . '"') == $post_id) {
-          //   // echo 'Form id: ' . $post_id . '<br>';
-          //   $font_end = '<strong><a href="' . $page_permalink . '" target="_blank">' . $page_title . '</a></strong>' . $page_status . '</br>';
-          //   // $back_end = '<strong><a href="' . $page_edit_link . '" target="_blank"> Editor page: ' . $page_title . '</a></strong></br></br>';
-          //   echo $font_end;
-          //   // echo $back_end;
-          // }
+            $page_status = $status_labels[$page_statuses[$page_id]] ?? '<span style="padding-left:0.2rem;color:darkorange;"><small> (Draft) </small></span>';
+            if (strpos($page_contents[$page_id], 'data-form-id="' . $post_id . '"') !== false) {
+              $font_end = '<strong><a href="' . $page_permalink . '" target="_blank">' . $page_title . '</a></strong>' . $page_status . '</br>';
+              echo $font_end;
+            }
         }
       }
-      else {
-        exit;
-      }
-    break;
+  break;
   }
 }
 add_action('manage_leads-form_posts_custom_column', __NAMESPACE__ . '\\leads_custom_column', 10, 2);
-
