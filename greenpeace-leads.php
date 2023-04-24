e7aaa00e32093a416fb56f03bbc56cad3fe2af9a
ellen <ellen@simmalugnt.se>
2023-04-24 10:26:19 +0200

    Update plugin version.

--- greenpeace-leads.php
+++ greenpeace-leads.php
@@ -4,7 +4,7 @@
  * Plugin Name: Greenpeace - Leads
  * Description: Petition with Gutenberg blocks
  * Plugin URI:
- * Version: 1.2.33
+ * Version: 1.2.34
  * Php Version: 7.0
  *
  * Author: Simma Lugnt
