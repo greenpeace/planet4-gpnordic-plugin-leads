5816e401ee1c466bfccd3761f7b2053a30700bb3
Ellen <ellen@simmalugnt.se>
2023-04-27 15:57:54 +0200

    Remake custom ask button fields into a repeater, reflect change in markup on custom ask partial.

--- parts/form/custom_ask.php
+++ parts/form/custom_ask.php
@@ -1,7 +1,13 @@
 <div class="leads-form__custom-ask">
   <h4><?php echo $headline; ?></h4>
   <?php echo $description; ?>
-  <a @click="completeMultistep(<?php echo $step_index; ?>)" class="button button--share" href="<?php echo $button_url; ?>" target="_blank"><?php echo $button_caption; ?></a>
+  <?php if ($buttons) : ?>
+    <div class="button-container">
+      <?php foreach ($buttons as $button) : ?>
+        <a @click="completeMultistep(<?php echo $step_index; ?>)" class="button button--share" href="<?php echo $button['button_url']; ?>" style="background-color: <?php echo $button['button_color']; ?>!important; color: <?php echo $button['button_text_color']; ?>!important;" target="_blank"><?php echo $button['button_caption']; ?></a>
+      <?php endforeach; ?>
+    </div>
+  <?php endif; ?> 
   <?php 
     $prev_next_data = array('step_index' => $step_index);
     GPPL4\get_partial("form/prev_next", $prev_next_data);  
