4899e2c4dc99e19eaeff7886a3d3bae1aecc6a24
Ellen <ellen@simmalugnt.se>
2023-04-27 15:29:07 +0200

    Add function to set step as skipped if user picks 'no thank you' option on thank you page.

--- parts/form/thank_you.php
+++ parts/form/thank_you.php
@@ -6,7 +6,7 @@ if ($form_type === 'multistep') : ?>
         <div class="leads-form__thank-you__text"><?php echo stripslashes($description); ?></div>
         <div class="button-container">
             <a class="button button--primary" @click="goToStep(<?php echo $share_go_to_step; ?>)"><?php echo $share_button_caption; ?></a> 
-            <a class="button button--secondary" @click="goToStep(<?php echo $skip_go_to_step; ?>)"><?php echo $skip_button_caption; ?></a>
+            <a class="button button--secondary" @click="disagreeToShare(<?php echo $share_go_to_step; ?>, <?php echo $skip_go_to_step; ?>)"><?php echo $skip_button_caption; ?></a>
         </div> 
         <?php 
             $prev_next_data = array('step_index' => 0);
