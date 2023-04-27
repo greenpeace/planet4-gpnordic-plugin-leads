7cf7008272d5d34c8610024c642dad3508d42178
Ellen <ellen@simmalugnt.se>
2023-04-27 16:59:52 +0200

    Make Skip and Go back strings translateable in options.

--- parts/form/prev_next.php
+++ parts/form/prev_next.php
@@ -1,15 +1,23 @@
+<?php 
+$form_fields_translations = get_field('form_fields_translations', 'options');
+
+$skip_caption = $form_fields_translations['multistep_skip_step'] ?: 'Skip that';
+$back_caption = $form_fields_translations['multistep_go_back_step'] ?: 'Go back';
+
+?>
+
 <div class="leads-form__prev-next">
   <nav>
     <ul>
       <li v-show="!isFirst(<?php echo $step_index; ?>)">
         <button @click="prevStep()">
           <?php GPPL4\svg_icon('chevron--left'); ?>
-          Go back
+          <?php echo $back_caption; ?>
         </button>
       </li>
       <li v-show="!wasCompleted(<?php echo $step_index; ?>) && !isLast(<?php echo $step_index; ?>)">
         <button @click="nextStep()">
-          Skip that
+          <?php echo $skip_caption; ?>
           <?php GPPL4\svg_icon('chevron--right'); ?>
         </button>
       </li>
