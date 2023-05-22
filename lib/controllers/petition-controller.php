7c10b3a5df91005fb9ca193056b672574df9ed70
Jens Waern <jens@simmalugnt.se>
2023-05-22 14:18:06 +0200

    Add caching for petition locations data and clear all cache on post save

--- lib/controllers/petition-controller.php
+++ lib/controllers/petition-controller.php
@@ -4,6 +4,8 @@ namespace GPLP\Controllers;
 
 class PetitionController
 {
+  public static $transient_prefix = 'petition_locations_';
+
   static function parse_page($page_id)
   {
     return array(
@@ -16,9 +18,30 @@ class PetitionController
   {
     // Use wpdb query to get IDs of pages that have the petition shortcode
     $id = $request['id'];
+    // Check if these is a transient for this petition
+    $transient_name = self::$transient_prefix . $id;
+    $transient = get_transient($transient_name);
+    if ($transient) {
+      return $transient;
+    }
+    // If not, query the database
     global $wpdb;
     $page_ids = $wpdb->get_col($wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE post_content LIKE '%%<!-- wp:acf/leads-form {%\"form\": %d,%} /-->%%' AND post_status IN ('publish','draft','auto-draft','pending','future','private');", $id));
-    return array_map("self::parse_page", $page_ids);
+    // Set transient for 1 hour
+    $response = array_map("self::parse_page", $page_ids);
+    set_transient($transient_name, $response, 60 * 60);
+    return array_map("self::parse_page", $response);
+  }
+
+  public static function clear_all_petition_locations_transients($post_id)
+  {
+    // If this is just a revision, don't do anything
+    if (wp_is_post_revision($post_id)) {
+      return;
+    }
+    global $wpdb;
+    $wpdb->query("DELETE FROM $wpdb->options WHERE option_name LIKE '_transient_" . self::$transient_prefix . "%';");
+    $wpdb->query("DELETE FROM $wpdb->options WHERE option_name LIKE '_transient_" . self::$transient_prefix . "%';");
   }
 
   public static function register_api_routes()
@@ -30,4 +53,6 @@ class PetitionController
     ));
   }
 }
+
 add_action('rest_api_init', __NAMESPACE__ . '\\PetitionController::register_api_routes');
+add_action('save_post', __NAMESPACE__ . '\\PetitionController::clear_all_petition_locations_transients');
