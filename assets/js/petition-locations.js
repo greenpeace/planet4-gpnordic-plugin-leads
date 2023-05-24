b3571338b3b85dc5c451d4e1f946634e8cba85c2
elle <svilena_koleva@yahoo.com>
2023-05-24 16:30:55 +0200

    Country selector added for petition locationss

--- assets/js/petition-locations.js
+++ assets/js/petition-locations.js
@@ -12,7 +12,7 @@ jQuery(function ($) {
       mounted() {
         // Use jQuery to fetch the data from the server
         $.ajax({
-          url: `/wp-json/gplp/v2/petition_locations/${this.id}`,
+          url: `/${window.location.pathname.split('/')[1]}/wp-json/gplp/v2/petition_locations/${this.id}`,
           method: "GET",
           beforeSend: (xhr) => {
             xhr.setRequestHeader("X-WP-Nonce", gplp.nonce);
